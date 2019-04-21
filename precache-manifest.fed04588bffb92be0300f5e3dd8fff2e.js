79db591ed8c71db7ee64a1ba8379e5f2acb26535
cboetto <cmitchellboetto@gmail.com>
2019-04-21 19:49:45 -0400

    Updates

---
+++ precache-manifest.fed04588bffb92be0300f5e3dd8fff2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8d437905e61dce6845a9",
+    "url": "/static/js/main.8d437905.chunk.js"
+  },
+  {
+    "revision": "c064b0bcd4024bc6b1c9",
+    "url": "/static/js/2.c064b0bc.chunk.js"
+  },
+  {
+    "revision": "8d437905e61dce6845a9",
+    "url": "/static/css/main.f2d59a11.chunk.css"
+  },
+  {
+    "revision": "6abf45d0d7d7694e64ef8b815798c4a5",
+    "url": "/index.html"
+  }
+];
